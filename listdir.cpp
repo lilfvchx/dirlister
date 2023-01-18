fc138d9dd0dbc70bf057b9583da61ad0f6fc60d8
lilfvchx <emiperalta256@gmail.com>
2023-01-18 14:59:50 -0300

    arreglo de formateo de codigo en tabla

--- listdir.cpp
+++ listdir.cpp
@@ -26,15 +26,17 @@ void list_files(string dir, string tab, ofstream &out) {
         if (entry->d_type == DT_DIR) {
             // Si no es el directorio actual o el directorio padre
             if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-                // Imprime el nombre del directorio y llama recursivamente a la funcion
-                out << tab << entry->d_name << "/" << endl;
+                // Llama recursivamente a la funcion
                 list_files(dir + "/" + entry->d_name, tab + "  ", out);
             }
         }
         // Si es un archivo
         else {
-            // Imprime el nombre del archivo
-            out << tab << entry->d_name << endl;
+            // escribe la fila de la tabla con el nombre del archivo y su ruta
+            out << "<tr>" << endl;
+            out << "<td>" << entry->d_name << "</td>" << endl;
+            out << "<td>" << dir << "</td>" << endl;
+            out << "</tr>" << endl;
         }
     }
     // Cierra el directorio
@@ -60,27 +62,28 @@ int main() {
     cin >> output_file;
 
     // Flujo de salida para escribir en un archivo
-ofstream out;
-// Abre el archivo especificado
-out.open(output_file);
-if(!out) {
-    cout << "Error opening " << output_file;
-    return -1;
-}
+    ofstream out;
+    // Abre el archivo especificado
+    out.open(output_file);
 
-// Escribe la estructura básica de un documento html en el archivo de salida
+    // Si no se puede abrir el archivo, muestra un error
+    if(!out) {
+        cout << "Error opening " << output_file;
+        return -1;
+    }
+
+    // Escribe la estructura básica de un documento html en el archivo de salida
 out << "<html>" << endl;
 out << "<head>" << endl;
 out << "<title> Directory Listing </title>" << endl;
 out << "</head>" << endl;
 out << "<body>" << endl;
 out << "<h1> Directory Listing </h1>" << endl;
-out << "<table>" << endl;
+out << "<table border='1'>" << endl;
 out << "<tr>" << endl;
 out << "<th> File Name </th>" << endl;
 out << "<th> File Path </th>" << endl;
 out << "</tr>" << endl;
-
 // Llama a la funcion para listar los archivos y escribir los resultados en el archivo de salida
 list_files(dir, "", out);
 
@@ -93,3 +96,6 @@ out.close();
 cout << "Directory listing saved to " << output_file << endl;
 return 0;
 }
+
+En este código se ha agregado la propiedad border='1' a la tabla para mostrar los bordes de las celdas y también se ha modificado la forma en que se escribe la información en la tabla, de tal manera que el nombre del archivo va en la primera columna y su ruta completa va en la segunda columna.
+
